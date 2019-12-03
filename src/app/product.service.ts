ac883a97c9c9cc92b90af8c8b2eb311a0d7f36b2
mansi <koshtamini@gmail.com>
2019-12-03 06:02:31 +0530

    added new ip

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProductService {
 
-  url = 'http://172.18.4.59:9898/product'
+  url = 'http://192.168.1.33:9898/product'
   constructor(private http:HttpClient) { }
 
   getProducts(){
